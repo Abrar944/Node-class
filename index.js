22b7aa57cfee51bf44eef52700fdb9fb943dbdf0
Mohammed Abrar Ahmed <70652985+Abrar944@users.noreply.github.com>
2025-03-04 22:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Welcome to Tutsmake first Node JS app! Keep things Jiggy ;-) "));
+app.get('/', (req, res) => res.send("Congratulations! You have deployed your first Node.js app! ;-) "));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
