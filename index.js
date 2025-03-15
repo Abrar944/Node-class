67683b327a64a419bdd894bec4343250a9c81151
Mohammed Abrar Ahmed <70652985+Abrar944@users.noreply.github.com>
2025-03-15 16:16:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const app = express()
-const port = 3000;
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => res.send("Congratulations! You have deployed your first Node.js app! ;-) "));
+app.get("/", (req, res) => {
+    res.send("Hello from Docker!");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
