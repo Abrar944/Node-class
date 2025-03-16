4843d009edc5c8c205dd5a671cfc35b67d4cc1d3
Mohammed Abrar Ahmed <70652985+Abrar944@users.noreply.github.com>
2025-03-17 02:57:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from Docker!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
